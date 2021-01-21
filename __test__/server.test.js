af37cb6d0e21f212e669c1c2ddaaac64127f465a
Ahmed Alsaqaf <ahmadalsaggaf30@gmail.com>
2021-01-21 08:31:01 -0500

    path typo

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/server/server.js');
+const app = require('../src/server/index.js');
 
 describe('to test the root path', () => {
   test('It should response the GET method', async () => {
